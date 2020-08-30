731a20773c03da6b462bf1a1914820b292483ceb
DhrubojyotiBis1 <bisdhruv1998@gmail.com>
2020-08-30 15:44:17 +0530

    Created general purpos blockchain

---
+++ src/block_chain.py
@@ -0,0 +1,58 @@
+import datetime
+import hashlib
+
+class Blockchain():
+    def __init__(self):
+        self.chain = []
+        self.create_blockchain(proof = 1, previous_hash = '0')
+    
+    def create_blockchain(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'previous_hash': previous_hash,
+                 'proof': proof
+        }     
+        self.chain.append(block)
+        return block
+    
+    def get_previous_block(self):
+        return self.chain[-1]
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while not check_proof:
+            problem = new_proof**2 - previous_proof**2 #problem should not be symetric
+            hash_operation = hashlib.sha256(str(problem).encode()).hexdigest() 
+            if hash_operation[:4] ==  '0000':
+                #more the number of leading zeros smaller the target and harder to get the proof of work 
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+    
+    def is_valid_chain(self, chain):
+        previous_block = chain[0]
+        previous_index = 1
+
+        while previous_index < len(self.chain):
+            #check if previous_hash is equal to previous block hash
+            block = chain[previous_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            #check if hash_operation of previous_proof and current_proof is below the target
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                return False
+            #update
+            previous_block = block
+            block_index += 1
+        
+        return True
+    
